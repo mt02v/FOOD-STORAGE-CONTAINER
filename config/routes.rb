a2d56294c13d6233512bd07ea5bccc7deb949826
mt02v <141305346+mt02v@users.noreply.github.com>
2023-11-06 16:36:53 +0000

    [Add] topページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    root "top#index"
+    root to: "top#index"
      devise_for :users, controllers: {
        registrations: "users/registrations",
        passwords: "users/passwords"
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
  resources :users, only: [:show]
  resources :posts
  resources :memos
-
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+ end
 end
 
