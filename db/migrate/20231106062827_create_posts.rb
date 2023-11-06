1c749140051a7d17514bed97c87d9fab126556a0
mt02v <141305346+mt02v@users.noreply.github.com>
2023-11-06 08:20:22 +0000

    [Add] model

---
+++ db/migrate/20231106062827_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.string :product
+      t.string :memo
+      t.data :start_time
+
+      t.timestamps
+    end
+  end
+end
