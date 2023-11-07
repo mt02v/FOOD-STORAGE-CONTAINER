bc45921375e2838e416b0fa16b279cf4256f7699
mt02v <141305346+mt02v@users.noreply.github.com>
2023-11-07 14:42:12 +0000

    [Add] 微修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+  belongs_to :user, optional: true
+  has_many :post_genres, dependent: :destroy
+  has_many :genres, through: :post_genres, dependent: :destory
+
+  validates :product, presence: true, length: { maximum: 30 }
+  validates :start_time, presence: true
 end
