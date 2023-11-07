7610f212ac0f1b9d159b216fbd69175407c4d287
mt02v <141305346+mt02v@users.noreply.github.com>
2023-11-07 06:41:16 +0000

    [Add] 買い物リスト

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,2 +1,50 @@
 class MemosController < ApplicationController
-  en
+
+before_action :set_memo, only: %i[edit update destroy]
+before_action :move_to_signed_in, except: [:top]
+
+def index
+  @memos = Memo.where(user_id: current_user.id)
+  @memo = Memo.new
+end
+
+def create
+  @memo = Memo.new(memo_params)
+  if @memo.save
+    redirect_to memos_path, notice: "追加しました"
+  else
+    flash.now[:alert] = "追加に失敗しました"
+    redirect_to memos_path
+  end
+end
+
+def edit
+end
+
+def update
+  if @memo.update(memo_params)
+    redirect_to memos_path, notice: "変更しました"
+  else
+    flash.now[:alert] = "変更に失敗しました"
+    render :edit
+  end
+end
+
+def destroy
+  @memo.destroy!
+  redirect_to memos_path, alert: "削除しました"
+end
+
+private
+
+ def set_memo
+  @memo = Memo.find(params[:id])
+ end
+
+ def move_to_signed_in
+  unless user_signed_in?
+    #サインインしていないユーザーはログインページが表示される
+    redirect_to "/users/sign_in"
+  end
+ end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_06_064303) do
+ActiveRecord::Schema.define(version: 2023_11_07_063204) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2023_11_06_064303) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
