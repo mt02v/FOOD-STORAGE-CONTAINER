06e2b2c4221efb9ba9ccb35b7e344d53eae01e8b
mt02v <141305346+mt02v@users.noreply.github.com>
2023-11-07 08:13:29 +0000

    [Add] 在庫一覧

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,59 @@
 class PostsController < ApplicationController
+
+  before_action :set_post, only: %i[edit update destroy]
+  before_action :move_to_signed_in, except: [:top]
+
+  def index
+    @genres = Genre.all
+    @posts = params[:name].present? ? Genre.find(params[:name]).posts.where(user_id: current_user.id) : Post.where(user_id: current_user.id)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to posts_path, notice: "#{@post.product}を追加しました"
+    else
+      flash.now[:alert] = "追加に失敗しました"
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "#{@post.product}を更新しました"
+    else
+      flash.now[:alert] = "更新しました"
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy!
+    redirect_to post_path, alert: "#{@post.product}を削除しました"
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.requore(:post).permit(:product, :memo, :start_time, genre_id: [], user_id: @current_user.id)
+  end
+
+  def move_to_signed_in
+    unless user_signed_in?
+      #サインインしていないユーザーはログインページが表示される
+      redirect_to "/users/sign_in"
+    end
+  end
 end
