0df2e50ad8c2c30a891cc134b7017e340de49cc9
mt02v <141305346+mt02v@users.noreply.github.com>
2023-11-14 15:25:00 +0000

    [Add] 修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    root to: "top#index"
-     devise_for :users, controllers: {
-       registrations: "users/registrations",
-       passwords: "users/passwords",
-       sessions: "users/sessions"
+  root to: "top#index"
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    passwords: "users/passwords",
+    sessions: "users/sessions"
   }
 
  get "/index/:genre", to: "items#index"
