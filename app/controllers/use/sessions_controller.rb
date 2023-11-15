3c0d8d0e9334a0efb23e6e01e72682d87147bc7a
mt02v <141305346+mt02v@users.noreply.github.com>
2023-11-15 13:13:06 +0000

    [Add] ゲストログイン 修正

--- app/controllers/use/sessions_controller.rb
+++
@@ -1,8 +0,0 @@
-class Users::SessionsController < Devise::SessionsController
-  def guest_sign_in
-    # ゲストアカウントでログイン
-    sign_in User.guest
-    #トップページへリダイレクト
-    redirect_to posts_path, notice: "ゲストユーザーとしてログインしました。"
-  end
-end
